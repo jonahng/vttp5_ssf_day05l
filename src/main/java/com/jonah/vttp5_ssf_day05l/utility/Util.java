337b7706e0429933f0fb9b95d388a235ab9a2957
jonahng <nyhj1998@gmail.com>
2024-11-22 11:26:54 +0800

    basic redisconfig

---
+++ src/main/java/com/jonah/vttp5_ssf_day05l/utility/Util.java
@@ -0,0 +1,6 @@
+package com.jonah.vttp5_ssf_day05l.utility;
+
+public class Util {
+    public static final String template01 = "template01";
+    public static final String template02 = "template02";
+}
